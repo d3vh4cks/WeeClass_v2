e21bed596c81813f9d80320623278d47e775d694
fbrjsah0725@gmail.com <fbrjsah0725@gmail.com>
2024-09-11 15:50:22 +0900

    Refactor: load_dotenv

--- config.py
+++ config.py
@@ -1,6 +1,13 @@
 import os
+from dotenv import load_dotenv
 
 class Config:
-  WEECLASS_DB = os.getenv('WEECLASS_DB')
-  WEECLASS_DB_USER = os.getenv('WEECLASS_DB_USER')
-  WEECLASS_DB_PW = os.getenv('WEECLASS_DB_PW')
+  WEECLASS_DB = ''
+  WEECLASS_DB_USER = ''
+  WEECLASS_DB_PW = ''
+  
+  def load_config():
+    load_dotenv()
+    Config.WEECLASS_DB = os.getenv('WEECLASS_DB') 
+    Config.WEECLASS_DB_USER = os.getenv('WEECLASS_DB_USER')
+    Config.WEECLASS_DB_PW = os.getenv('WEECLASS_DB_PW')
--- run.py
+++ run.py
@@ -1,8 +1,5 @@
-from dotenv import load_dotenv
-
-load_dotenv()
-
-from app import app
+from app import app, Config
 
 if __name__ == '__main__':
+  Config.load_config()
   app.run(host='127.0.0.1', port=4445)
